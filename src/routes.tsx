c901fa07a94c7022e3a6084db5ed6d5bacc44aa6
Ian Felix <62842327+ianfelix@users.noreply.github.com>
2020-10-12 20:10:10 -0300

    finish day 01

---
+++ src/routes.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Landing from './pages/Landing';
+import OrphanagesMap from './pages/OrphanagesMap';
+
+const MainRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='/app' element={<OrphanagesMap />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default MainRoutes;
